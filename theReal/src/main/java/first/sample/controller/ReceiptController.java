2baef1ebc0d9831d5303a66235adf7aeded25ec7
thereal_bolee <bolee@realmkt.co.kr>
2018-09-06 16:38:08 +0900

    2018.09.06 두번째 마스터

--- theReal/src/main/java/first/sample/controller/ReceiptController.java
+++ theReal/src/main/java/first/sample/controller/ReceiptController.java
@@ -2697,7 +2697,7 @@ public class ReceiptController {
 		try {
 			if (var.find("salesInfo.salesType").toString().equals("RCP01")) {
 				log.debug("====================================================");
-				log.debug("■■■■■■■■■■■■■■■■■RCP01 전자영수증 승인건■■■■■■■■■■■■■789■■■■■");
+				log.debug("■■■■■■■■■■■■■■■■■RCP01 전자영수증 승인건■■■■■■■■■■■■■7890■■■■■");
 				
 				delMap.put("salesType", "RCP01");
 				
